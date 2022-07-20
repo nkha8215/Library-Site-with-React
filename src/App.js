4bed758d91f92c779174d1f8d3340a64f678270d
Nayab <mrnayabkhan@hotmail.com>
2022-07-20 23:43:08 +1000

    progree on library e-commerce  site using react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import Nav from './components/Nav'
+import React, {useState, useEffect} from 'react'
+import Footer from './Footer'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Books from './pages/Books'
+import { books } from "./data"
+import BookInfo from './pages/BookInfo'
+import Cart from './pages/Cart'
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, book])
+  }
+
+  useEffect(() => {
+    console.log(cart)
+  }, [cart])
+
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Route path="/" exact component={Home} />
+      <Route path="/books" exact render={() => <Books books={books} />} />
+      <Route path="/books/:id" render={() => <BookInfo books={books} addToCart={addToCart} cart={cart} />} />
+      <Route path="/cart" render={() => <Cart books={books}/>} />
+      <Footer />
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/ui/Book.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom'
+import Rating from './Rating';
+import Price from './Price';
+
+const Book = ({ book }) => {
+
+    return (
+        <div className="book">
+            <Link to={`/books/${book.id}`}>
+                <figure className="book__img--wrapper">
+                    <img 
+                    src={book.url}
+                    alt="" 
+                    className="book__img" 
+                    />
+                </figure>
+            </Link>
+            <div className="book__title">
+                <Link to={`/books/${book.id}`} className='book__title--link'>
+                    {book.title}
+                </Link>
+            </div>
+            <Rating rating={book.rating} />
+            <Price originalPrice={book.originalPrice} salePrice={book.salePrice}/>
+        </div>
+    );
+}
+
+export default Book;
